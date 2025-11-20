018a8e492de436f03f87a2610c06f0c3a324981b
sachin <sachinprajapati0203@gmail.com>
2025-11-20 22:23:17 +0530

    optimize

--- 02bgChanger/src/App.jsx
+++ 02bgChanger/src/App.jsx
@@ -2,26 +2,29 @@ import { useState } from 'react'
 
 function App() {
   const [color, setColor] = useState("violet");
+  const [active, setActive] = useState(0);
+  const buttons = [ 'red', 'green', 'yellow', 'blue', 'lightgreen', 'purple', 'violet', 'lightblue', 'lime', 'gray'];
+
+  const handleClick = (index,btn)=>{
+    setActive(index);
+    setColor(btn)
+  }
 
   return (
-    <div className="w-auto h-screen duration-200" style={{backgroundColor: color}}
+    <div className="w-auto h-screen duration-400" style={{backgroundColor: color}}
     >
-      <div className='fixed flex flex-wrap bg-white w-full h-14  justify-evenly bottom-12 mx-2 shadow-md rounded-2xl '>
-        <button onClick={()=> {setColor("red")}}  className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "red"}}>Red</button>
+      <div className='fixed flex flex-wrap w-full bg-zinc-300  justify-evenly bottom-12 shadow-md rounded-md py-2 gap-2 ' >
+        {buttons.map((btn,index)=>(
 
-        <button onClick={()=> {setColor("green")}}  className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "green"}}>Green</button>
-
-        <button onClick={()=> {setColor("yellow")}}  className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "Yellow"}}>Yellow</button>
-
-        <button onClick={()=> {setColor("blue")}}  className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "Blue"}}>Blue</button>
-
-        <button onClick={()=> {setColor("purple")}}  className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "purple"}}>Purple</button>
-
-        <button onClick={()=> {setColor("violet")}}  className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "violet"}}>Violet</button>
-
-        <button onClick={()=> {setColor("lightblue")}} className='outline-none mt-2 h-10 px-4 border-2  rounded-xl shadow-sm text-white' style={{backgroundColor: "lightblue"}}>Lightblue</button>
-
-        
+          <button  
+            key={index} 
+            className='h-8 px-8  capitalize rounded-full shadow-sm cursor-pointer active:scale-95 duration-200  ' 
+            onClick={()=>handleClick(index,btn)}
+            style={{backgroundColor: btn}} 
+          >
+            {btn}
+          </button>
+        ))}
       </div>
 
     </div>
